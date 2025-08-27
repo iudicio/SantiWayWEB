b82407b5450a4887330ae3bc4d577fc6cfbb60a5
DSOF <leytenantmorozov@yandex.ru>
2025-08-27 22:23:51 +0300

    Добавил в requirements библиотеки python. Доработал API: запись данных в ElasticSearch, получение данных из ElasticSearch. В приложении users написал логику выдачи API ключа

--- SantiWayWEB/urls.py
+++ SantiWayWEB/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import DeviceViewSet
+from users.views import APIKeyViewSet
+
+router = DefaultRouter()
+
+router.register(r"devices", DeviceViewSet, basename="devices")
+router.register(r"api-keys", APIKeyViewSet, basename="api-keys")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,19 +1,33 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .serializers import DeviceSerializer
 from elasticsearch import Elasticsearch
+from .serializers import DeviceSerializer
+from os import getenv
+import uuid
 
-es = Elasticsearch(hosts=["http://localhost:9200"])
-INDEX_PREFIX = "way-*"
+ES_HOST = getenv("ES_HOST", None)
+ES_USER = getenv("ES_USER", None)
+ES_PASSWORD = getenv("ES_PASSWORD", None)
+ES_INDEX_PREFIX = getenv("ES_INDEX_PREFIX", None)
+ES_MAX_DOCS_PER_INDEX = getenv("ES_MAX_DOCS_PER_INDEX", None)
 
-class DeviceViewSet(viewsets):
+es = Elasticsearch(
+            hosts = [ES_HOST], basic_auth=(ES_USER, ES_PASSWORD)
+        )
+
+
+class DeviceViewSet(viewsets.ViewSet):
+    serializer_class = DeviceSerializer
+    lookup_field = "device_id"
+
+
     def list(self, request, *args, **kwargs):
         must_filters = []
-
+        global es
         for field, value in request.query_params.items():
             # Диапазоны (например: ?timestamp__gte=2025-01-01T00:00:00)
-            if "" in field:
-                field_name, op = field.split("", 1)
+            if "__" in field:
+                field_name, op = field.split("__", 1)
                 if op in ["gte", "lte", "gt", "lt"]:
                     must_filters.append({
                         "range": {field_name: {op: value}}
@@ -33,7 +47,27 @@ class DeviceViewSet(viewsets):
 
         query = {"query": {"bool": {"must": must_filters}}} if must_filters else {"query": {"match_all": {}}}
 
-        res = es.search(index=INDEX_PREFIX, body=query, size=100)
+        res = es.search(index=ES_INDEX_PREFIX, body=query, size=100)
 
         return Response([hit["_source"] for hit in res["hits"]["hits"]])
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        es = self.get_es()
+        doc = serializer.validated_data
+
+        # уникальный id: device_id + timestamp, иначе uuid
+        if "detected_at" in doc and doc["detected_at"]:
+            doc_id = f"{doc['device_id']}_{int(doc['detected_at'].timestamp())}"
+        else:
+            doc_id = str(uuid.uuid4())
+
+        # всегда пишем в alias "way"
+        es.index(index="way", id=doc_id, document=doc)
+
+        return Response({"id": doc_id, **doc}, status=status.HTTP_201_CREATED)
+
+        
     
