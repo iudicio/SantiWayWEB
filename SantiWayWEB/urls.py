ee92885ad0d717e2958e7571d1ec19072f05b87a
Svetik-style <wat76erfa.llvie39wd.anke@mail.ru>
2025-09-03 17:11:54 +0300

    Update urls.py

--- SantiWayWEB/urls.py
+++ SantiWayWEB/urls.py
@@ -4,6 +4,12 @@ from rest_framework.routers import DefaultRouter
 from api.views import DeviceViewSet
 from users.views import APIKeyViewSet
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 router = DefaultRouter()
 
 router.register(r"devices", DeviceViewSet, basename="devices")
@@ -11,5 +17,11 @@ router.register(r"devices/api-key", APIKeyViewSet, basename="api-keys")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # JSON-схема OpenAPI (генерится на лету)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # ReDoc (альтернативная документация)
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/", include(router.urls)),
 ]
