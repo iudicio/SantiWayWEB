da35dd6cd13b73471daebaba86eff2ada6385747
rera <zxa2484@gmail.com>
2025-10-10 20:51:53 +0300

    feat(notifications): add auto-notification on anomaly detection via signals

--- polygons/apps.py
+++ polygons/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PolygonsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polygons'
+    
+    def ready(self):
+        """Импортируем сигналы при запуске приложения"""
+        import polygons.signals  # noqa
